05ed74bce778e9b14725cee5180a90647b517ee4
Dylan Green <dylan.green61@gmail.com>
2026-06-05 18:09:56 -0700

    "remove a bunch of deleted shit"

--- src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAccessToken } from '../controllers/auth.controller.js';
-
-// Mounted at '/auth' in index.ts.
-const router: Router = Router();
-
-router.get('/token', getAccessToken); // GET /auth/token
-
-export default router;
